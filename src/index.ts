69275eeb78a562224b78643c24a28376f090a643
Noxturnix <25237131+Noxturnix@users.noreply.github.com>
2021-07-18 22:31:27 -0700

    added simple bot script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello Psych2Goers!");
+import {config as dotenvConfig} from "dotenv";
+import {Client, ClientUser, Intents} from "discord.js";
+
+dotenvConfig(); // read environment variables from .env file
+
+// DiscordJS documentations can be found here
+// https://discord.js.org/#/docs/main/stable/general/welcome
+
+const client = new Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as "${(client.user as ClientUser).tag}"`);
+});
+
+client.on("error", (error: Error) => {
+  console.warn(`An error occurred: ${error.message}`);
+});
+
+if (process.env.BOT_TOKEN)
+  // check if `BOT_TOKEN` environment variable is defined
+  client.login(process.env.BOT_TOKEN).catch((error: Error) => {
+    console.error(`Error: ${error.message}`); // output an error if there's a problem when logging in
+  });
+else {
+  console.error("Environment variable `BOT_TOKEN` is not defined");
+  client.destroy();
+}
+
+process.on("SIGINT", () => {
+  console.log("SIGINT received, destroying client...");
+  client.destroy();
+  process.exit(0);
+});
