d8eba531b8753a5d8fb7229f77637c339a15426e
Noxturnix <25237131+Noxturnix@users.noreply.github.com>
2021-07-19 22:57:36 -0700

    better command handler

---
+++ src/commands/messageCommands.ts
@@ -0,0 +1,32 @@
+import { Message } from "discord.js";
+
+interface MessageCommand {
+  command: string;
+  fn: (message: Message) => void | boolean;
+}
+
+interface MessageCommands {
+  userCommands: Array<MessageCommand>;
+  adminCommands: Array<MessageCommand>;
+}
+
+// all commands should have lower case
+
+export const messageCommands: MessageCommands = {
+  userCommands: [
+    {
+      command: "hi psi",
+      fn: (message: Message) => {
+        message.reply("hey psych2goer!");
+      }
+    }
+  ],
+  adminCommands: [
+    {
+      command: "test admin",
+      fn: (message: Message) => {
+        message.reply("Test passed, you are an admin!");
+      }
+    }
+  ]
+};
--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,43 @@
-import { Message } from "discord.js";
+import { GuildMember, Message } from "discord.js";
+import { ADMIN_ROLE_ID } from "..";
+import { messageCommands } from "../commands/messageCommands";
 
 export const handleMessageEvent = (message: Message) => {
-  if (!message.author.bot) {
-    switch (message.content.toLowerCase()) {
-      case "hi psi":
-        message.reply("hi");
+  if (!message.author.bot && message.guild) {
+    let messageLower = message.content.toLowerCase();
+    let userCommandRan = false;
+
+    for (let i = 0; i < messageCommands.userCommands.length; i++) {
+      let messageCommand = messageCommands.userCommands[i];
+
+      if (messageLower === messageCommand.command) {
+        messageCommand.fn(message);
+        userCommandRan = true;
         break;
+      }
+    }
+
+    let adminFunction = null;
+
+    for (let i = 0; i < messageCommands.adminCommands.length; i++) {
+      let messageCommand = messageCommands.adminCommands[i];
+
+      if (messageLower === messageCommand.command) {
+        adminFunction = messageCommand.fn;
+        break;
+      }
+    }
+
+    if (!userCommandRan && adminFunction) {
+      if (ADMIN_ROLE_ID) {
+        if ((message.member as GuildMember).roles.cache.has(ADMIN_ROLE_ID)) {
+          adminFunction(message);
+        } else {
+          message.reply("You do not have permission to use the command");
+        }
+      } else {
+        message.reply("`ADMIN_ROLE_ID` environment variable is not defined");
+      }
     }
   }
 };
