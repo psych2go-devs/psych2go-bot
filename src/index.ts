4adcf093427086139ae60ece87710f9bf3ec0ac8
Noxturnix <25237131+Noxturnix@users.noreply.github.com>
2021-07-19 22:57:36 -0700

    better command handler

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { config as dotenvConfig } from "dotenv";
-import { Client, ClientUser, Intents } from "discord.js";
+import { Client, ClientUser, Intents, Snowflake } from "discord.js";
 import { handleMessageEvent } from "./events/messageCreate";
 
 dotenvConfig(); // read environment variables from .env file
@@ -7,9 +7,20 @@ dotenvConfig(); // read environment variables from .env file
 // DiscordJS documentations can be found here
 // https://discord.js.org/#/docs/main/master/general/welcome
 
+// Define functions
+
+export const destroyClientAndExit = (exitCode: number = 0) => {
+  client.destroy();
+  process.exit(exitCode);
+};
+
+// Initialize new client
+
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+
+// Handle events
 
 client.on("ready", () => {
   console.log(`Logged in as "${(client.user as ClientUser).tag}"`);
@@ -21,18 +32,25 @@ client.on("error", (error: Error) => {
 
 client.on("messageCreate", handleMessageEvent);
 
-if (process.env.BOT_TOKEN)
+// Handle environment variables
+
+if (!process.env.BOT_TOKEN)
   // check if `BOT_TOKEN` environment variable is defined
-  client.login(process.env.BOT_TOKEN).catch((error: Error) => {
-    console.error(`Error: ${error.message}`); // output an error if there's a problem when logging in
-  });
-else {
-  console.error("Environment variable `BOT_TOKEN` is not defined");
-  client.destroy();
-}
+  destroyClientAndExit();
+
+export const ADMIN_ROLE_ID = (
+  process.env.ADMIN_ROLE_ID ? process.env.ADMIN_ROLE_ID : ""
+) as Snowflake;
+
+// Login
+
+client.login(process.env.BOT_TOKEN).catch((error: Error) => {
+  console.error(`Error: ${error.message}`); // output an error if there's a problem when logging in
+});
+
+// Handle exit signal
 
 process.on("SIGINT", () => {
   console.log("SIGINT received, destroying client...");
-  client.destroy();
-  process.exit(0);
+  destroyClientAndExit();
 });
