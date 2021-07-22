8fc44770bc88ead415a973e50e3fa302f07bb13b
Noxturnix <25237131+Noxturnix@users.noreply.github.com>
2021-07-22 16:41:58 -0700

    added bomb command

--- src/commands/messageCommands.ts
+++ src/commands/messageCommands.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 
 interface MessageCommand {
   command: string;
@@ -9,6 +9,8 @@ interface MessageCommands {
   userCommands: Array<MessageCommand>;
   adminCommands: Array<MessageCommand>;
 }
+
+const commandPrefix = "psi!"; // This shouldn't be hard-coded, but anyway
 
 export const messageCommands: MessageCommands = {
   userCommands: [
@@ -21,9 +23,38 @@ export const messageCommands: MessageCommands = {
   ],
   adminCommands: [
     {
-      command: "test admin",
-      fn: (message, argv) => {
-        message.reply("Test passed, you are an admin!");
+      command: commandPrefix + "bomb",
+      fn: async (message, argv) => {
+        let replyUsage = () => {
+          message.reply(`Usage: ${commandPrefix}bomb <limit> [search]`);
+        };
+
+        if (argv.length) {
+          let fetchLimit = parseInt(argv[0]);
+
+          if (!isNaN(fetchLimit)) {
+            let fetchedMessages = await message.channel.messages.fetch({
+              before: message.id,
+              limit: fetchLimit
+            });
+
+            if (argv.length > 1) {
+              let searchString = argv.slice(1).join(" ").toLowerCase();
+              let matchMessages = fetchedMessages.filter((fetchedMessage) =>
+                fetchedMessage.content
+                  .trim()
+                  .toLowerCase()
+                  .includes(searchString)
+              );
+
+              (message.channel as TextChannel).bulkDelete(matchMessages);
+              message.reply(`Deleted ${matchMessages.size} message(s)`);
+            } else {
+              (message.channel as TextChannel).bulkDelete(fetchedMessages);
+              message.reply(`Deleted ${fetchedMessages.size} message(s)`);
+            }
+          } else replyUsage();
+        } else replyUsage();
       }
     }
   ]
