c887013ba6c78c04deac757a697691b263f3ef56
Noxturnix <25237131+Noxturnix@users.noreply.github.com>
2021-07-23 22:49:53 -0700

    less psi
    lol

--- src/commands/allMessages.ts
+++ src/commands/allMessages.ts
@@ -1,21 +1,5 @@
-import { GuildEmoji, Message } from "discord.js";
+import { Message } from "discord.js";
 
 export const handlerMessages = (message: Message) => {
   let msgLower = message.content.trim().toLowerCase();
-
-  if (
-    msgLower.includes("p") &&
-    msgLower.includes("s") &&
-    msgLower.includes("i")
-  ) {
-    let matchedPsiEmojis = message.client.emojis.cache.filter(
-      (emoji) => (emoji.name as string).toLowerCase() === "psi"
-    );
-
-    if (matchedPsiEmojis.size) {
-      let psiEmoji = matchedPsiEmojis.first() as GuildEmoji;
-
-      message.react(psiEmoji);
-    }
-  }
 };
