52d02a41c6f5e1e251160a10845fe74e2c9088ce
Noxturnix <noxturnix@noxt.us>
2023-11-04 17:56:40 -0700

    sticky message: initialization

--- src/event/onStart.ts
+++ src/event/onStart.ts
@@ -1,4 +1,45 @@
-import type { Client } from "discord.js";
+import type { Client, TextChannel } from "discord.js";
+import { ChannelType } from "discord.js";
 import { stickyMessages } from "../lib/stickyMessages";
 
-export default async (client: Client) => {};
+export default async (client: Client) => {
+  // Send a new sticky message if one is not found in last 5 messages on a channel
+  for (let channelId of stickyMessages.keys()) {
+    let channel = client.channels.cache.get(channelId);
+
+    if (channel === undefined) continue;
+
+    if (channel.type !== ChannelType.GuildText) continue;
+
+    // Get last 5 messages
+    let channelMessages = await (channel as TextChannel).messages.fetch({ limit: 5 });
+    let stickyMessage = stickyMessages.get(channelId);
+
+    if (stickyMessage === undefined) continue;
+
+    let stickyMessageContent = stickyMessage.getMessage();
+
+    for (let channelMessage of channelMessages.values()) {
+      if (
+        channelMessage.author.id === client.user?.id &&
+        channelMessage.content === stickyMessageContent
+      ) {
+        stickyMessage.setSentId(channelMessage.id);
+        break;
+      }
+    }
+
+    let foundSentId = stickyMessage.getSentId();
+
+    if (foundSentId === undefined) {
+      // No sticky message found
+      let sentStickyMessage = await channel.send(stickyMessageContent);
+      stickyMessage.setSentId(sentStickyMessage.id);
+    } else if (foundSentId !== channelMessages.first()?.id) {
+      // Found sticky message, but is not the last message on the channel
+      channel.messages.delete(foundSentId);
+      let sentStickyMessage = await channel.send(stickyMessageContent);
+      stickyMessage.setSentId(sentStickyMessage.id);
+    }
+  }
+};
--- src/lib/stickyMessages.ts
+++ src/lib/stickyMessages.ts
@@ -1,8 +1,36 @@
 import jsonStickyMessages from "../asset/sticky-messages.json";
 
-export const stickyMessages = new Map<string, string>();
+class StickyMessage {
+  #channelId: string;
+  #message: string;
+  #sentId: string | undefined;
+
+  constructor(channelId: string, message: string, sentId?: string) {
+    this.#channelId = channelId;
+    this.#message = message;
+    this.#sentId = sentId;
+  }
+
+  public getChannelId() {
+    return this.#channelId;
+  }
+  public getMessage() {
+    return this.#message;
+  }
+  public getSentId() {
+    return this.#sentId;
+  }
+
+  public setSentId(sentId: string) {
+    this.#sentId = sentId;
+  }
+}
+
+export const stickyMessages = new Map<string, StickyMessage>();
 
 for (let channelId of Object.keys(jsonStickyMessages)) {
   let message: string = (jsonStickyMessages as any)[channelId];
-  stickyMessages.set(channelId, message);
+  let stickyMessage = new StickyMessage(channelId, message);
+
+  stickyMessages.set(channelId, stickyMessage);
 }
